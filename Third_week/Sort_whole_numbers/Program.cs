1ef5522860eac822938769faace027b4be356714
Максим <kyngfy-panda@yandex.ru>
2018-10-09 23:07:22 +0500

    Использовал новый алгоритм
    Использовал цифровую сортировку, использующую  сортировку подсчетом, а также разбивал числа на 4 байта, использовал картежи и т.д.

--- Third_week/Sort_whole_numbers/Program.cs
+++ Third_week/Sort_whole_numbers/Program.cs
@@ -1,16 +1,12 @@
 ﻿namespace Third_week
 {
-    using System;
     using System.IO;
-    using System.Diagnostics;
     using System.Linq;
 
     class Program
     {
         static void Main()
         {
-            //Stopwatch timer = new Stopwatch();
-            //timer.Start();
             StreamReader input = new StreamReader("input.txt");
             StreamWriter output = new StreamWriter("output.txt");
             input.ReadLine();
@@ -29,64 +25,70 @@
             }
 
             input.Close();
-            int[] countNumbers = new int[24415];
+            (byte, byte, byte, byte)[] byteArray = new (byte, byte, byte, byte)[firstArray.Length * secondArray.Length * 2];
             for (int i = 0; i < firstArray.Length; i++)
             {
                 for (int j = 0; j < secondArray.Length; j++)
                 {
-                    countNumbers[(firstArray[i] * secondArray[j]) >> 16]++;
+                    byteArray[(i * secondArray.Length) + j].Item1 = (byte)((firstArray[i] * secondArray[j]) & 255);
+                    byteArray[(i * secondArray.Length) + j].Item2 = (byte)((firstArray[i] * secondArray[j]) >> 8 & 255);
+                    byteArray[(i * secondArray.Length) + j].Item3 =
+                        (byte)((firstArray[i] * secondArray[j]) >> 16 & 255);
+                    byteArray[(i * secondArray.Length) + j].Item4 =
+                        (byte)((firstArray[i] * secondArray[j]) >> 24 & 255);
                 }
             }
 
-            for (int i = 1; i < countNumbers.Length; i++)
+            int[] countNumbers = new int[1024];
+            for (int i = 0; i < byteArray.Length / 2; i++)
+            {
+                countNumbers[byteArray[i].Item1]++;
+                countNumbers[byteArray[i].Item2 + 256]++;
+                countNumbers[byteArray[i].Item3 + 512]++;
+                countNumbers[byteArray[i].Item4 + 768]++;
+            }
+
+            for (int i = 1; i < 256; i++)
             {
                 countNumbers[i] += countNumbers[i - 1];
+                countNumbers[i + 256] += countNumbers[i + 255];
+                countNumbers[i + 512] += countNumbers[i + 511];
+                countNumbers[i + 768] += countNumbers[i + 767];
             }
 
-            long[] finalArray = new long[firstArray.Length * secondArray.Length];
-            
-            for (int i = 0; i < firstArray.Length; i++)
+            for (int j = byteArray.Length / 2 - 1; j > -1; j--)
             {
-                for (int j = 0; j < secondArray.Length; j++)
-                {
-                    finalArray[countNumbers[(firstArray[i] * secondArray[j]) >> 16] - 1] =
-                        firstArray[i] * secondArray[j];
-                    countNumbers[(firstArray[i] * secondArray[j]) >> 16]--;
-                }
+                byteArray[countNumbers[byteArray[j].Item1] - 1 + byteArray.Length / 2] = byteArray[j];
+                countNumbers[byteArray[j].Item1]--;
             }
 
-            Array.Sort(finalArray);
+            for (int j = byteArray.Length - 1; j > byteArray.Length / 2 - 1; j--)
+            {
+                byteArray[countNumbers[byteArray[j].Item2 + 256] - 1] = byteArray[j];
+                countNumbers[byteArray[j].Item2 + 256]--;
+            }
+
+            for (int j = byteArray.Length / 2 - 1; j > -1; j--)
+            {
+                byteArray[countNumbers[byteArray[j].Item3 + 512] - 1 + byteArray.Length / 2] = byteArray[j];
+                countNumbers[byteArray[j].Item3 + 512]--;
+            }
+
+            for (int j = byteArray.Length - 1; j > byteArray.Length / 2 - 1; j--)
+            {
+                byteArray[countNumbers[byteArray[j].Item4 + 768] - 1] = byteArray[j];
+                countNumbers[byteArray[j].Item4 + 768]--;
+            }
+
             long sum = 0;
-            for (int i = 0; i < finalArray.Length; i += 10)
+            for (int i = 0; i < byteArray.Length / 2; i += 10)
             {
-                sum += finalArray[i];
+                sum += (byteArray[i].Item4 << 24) + (byteArray[i].Item3 << 16) + (byteArray[i].Item2 << 8)
+                       + byteArray[i].Item1;
             }
 
-            //long[][] finalArray = new long[countNumbers.Length][];
-            //for (int i = 0; i < countNumbers.Length; i++)
-            //{
-            //    finalArray[i] = new long[countNumbers[i]];
-            //}
-
-            //for (int i = 0; i < firstArray.Length; i++)
-            //{
-            //    for (int j = 0; j < secondArray.Length; j++)
-            //    {
-            //        finalArray[(firstArray[i] * secondArray[j]) >> 16][
-            //            countNumbers[(firstArray[i] * secondArray[j]) >> 16] - 1] = firstArray[i] * secondArray[j];
-            //        countNumbers[(firstArray[i] * secondArray[j]) >> 16]--;
-            //    }
-            //}
-
-            //for (int i = 0; i < finalArray.Length; i++)
-            //{
-            //    Array.Sort(finalArray[i]);
-            //}
-
-            //Console.WriteLine(timer.Elapsed);
             output.WriteLine(sum);
             output.Close();
         }
     }
 }
-
