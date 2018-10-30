3ba7df41ce44c6a8a4eae56ffadd125d5a3c35ed
Максим <kyngfy-panda@yandex.ru>
2018-10-31 01:25:50 +0500

    Доработал решение по гирлянде
    Добавил правильный выбор половины, а также учитываю прошлый результат для исключительных ситуаций с бесконечным стремлением к нулю слева

--- Sixth_week/Garland/Program.cs
+++ Sixth_week/Garland/Program.cs
@@ -8,7 +8,7 @@
     {
         private static double BuildGarland(int countNodes, double lastHeight, double currentHeight)
         {
-            for (; 0 < countNodes && currentHeight > 0; --countNodes)
+            for (; 0 < countNodes && currentHeight >= 0; --countNodes)
             {
                 double shelf = currentHeight;
                 currentHeight = currentHeight - (lastHeight - currentHeight - 2);
@@ -25,53 +25,50 @@
             int countNodes = int.Parse(tokens[0]) - 2;
             double firstHeight = double.Parse(tokens[1], CultureInfo.InvariantCulture),
                    lastBestHeight = firstHeight,
-                   bestStartHeight = firstHeight / 2,
+                   currentBestHeight = 0,
                    deviation = 0.0000000001,
-                   upBoardResult = 0,
-                   downBoardResult,
+                   currentResult = 0,
+                   lastResult = 0,
                    shelfHeight;
 
             input.Close();
-            while (0 > BuildGarland(countNodes: countNodes, lastHeight: firstHeight, currentHeight: bestStartHeight))
-            {
-                shelfHeight = bestStartHeight;
-                bestStartHeight += Math.Abs(lastBestHeight - bestStartHeight) / 2;
-                lastBestHeight = shelfHeight;
-            }
-
             bool isFind = false;
             while (!isFind)
             {
-                downBoardResult = BuildGarland(
-                    countNodes,
-                    firstHeight,
-                    bestStartHeight - (Math.Abs(lastBestHeight - bestStartHeight) / 2));
-
-                upBoardResult = BuildGarland(
-                    countNodes,
-                    firstHeight,
-                    bestStartHeight + (Math.Abs(lastBestHeight - bestStartHeight) / 2));
-
-                shelfHeight = bestStartHeight;
-                if (downBoardResult > 0 && downBoardResult < upBoardResult)
+                if (currentResult >= 0)
                 {
-                    bestStartHeight -= Math.Abs(lastBestHeight - bestStartHeight) / 2;
+                    lastResult = currentResult;
+                }
+
+                shelfHeight = currentBestHeight;
+                if (currentResult <= 0)
+                {
+                    currentBestHeight += Math.Abs(lastBestHeight - currentBestHeight) / 2;
+                    currentResult = BuildGarland(countNodes, firstHeight, currentBestHeight);
                 }
                 else
                 {
-                    bestStartHeight += Math.Abs(lastBestHeight - bestStartHeight) / 2;
+                    currentBestHeight -= Math.Abs(lastBestHeight - currentBestHeight) / 2;
+                    currentResult = BuildGarland(countNodes, firstHeight, currentBestHeight);
                 }
 
                 lastBestHeight = shelfHeight;
-
-                if (Math.Abs(downBoardResult - upBoardResult) < deviation)
+                if (Math.Abs(lastResult - currentResult) < deviation || Math.Abs(currentResult) < deviation)
                 {
                     isFind = true;
                 }
             }
 
             StreamWriter output = new StreamWriter("output.txt");
-            output.WriteLine(upBoardResult);
+            if (currentResult >= 0)
+            {
+                output.WriteLine(currentResult);
+            }
+            else
+            {
+                output.WriteLine(lastResult);
+            }
+
             output.Close();
         }
     }
