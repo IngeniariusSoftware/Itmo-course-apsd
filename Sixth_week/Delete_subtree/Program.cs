62de99b5a74e4c7fcb2835eb70ea90330e43ee53
Максим <kyngfy-panda@yandex.ru>
2018-10-30 11:58:39 +0500

    Инициализировал решение для удаления поддерьев

---
+++ Sixth_week/Delete_subtree/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Delete_subtree
+{
+    using System.IO;
+    using System.Threading;
+
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader input = new StreamReader("input.txt");
+            int[] nodes;
+            int length = int.Parse(input.ReadLine());
+            if (length > 0)
+            {
+                nodes = new int[length * 2];
+                string[] tokens;
+                for (int i = 0; i < length; i++)
+                {
+                    tokens = input.ReadLine().Split(' ');
+                    nodes[i * 2] = int.Parse(tokens[1]);
+                    nodes[i * 2 + 1] = int.Parse(tokens[2]);
+                }
+
+                Thread newThread = new Thread(BigStackMain, 100000000);
+                newThread.Start();
+            }
+            else
+            {
+                output.WriteLine(0);
+                output.Close();
+            }
+        }
+    }
+}
