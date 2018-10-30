b90a5f8aee1f2bf50ba9a698808ef8b9afa37c47
Максим <kyngfy-panda@yandex.ru>
2018-10-31 01:24:33 +0500

    Доработал решение по удалению поддеревьев
    Добавил работу с кортежами и словарь для быстрого доступа к элементам

--- Sixth_week/Delete_subtree/Program.cs
+++ Sixth_week/Delete_subtree/Program.cs
@@ -1,34 +1,73 @@
 ﻿namespace Delete_subtree
 {
+    using System.Collections.Generic;
     using System.IO;
-    using System.Threading;
 
     class Program
     {
-        static void Main()
-        {
-            StreamReader input = new StreamReader("input.txt");
-            int[] nodes;
-            int length = int.Parse(input.ReadLine());
-            if (length > 0)
-            {
-                nodes = new int[length * 2];
-                string[] tokens;
-                for (int i = 0; i < length; i++)
-                {
-                    tokens = input.ReadLine().Split(' ');
-                    nodes[i * 2] = int.Parse(tokens[1]);
-                    nodes[i * 2 + 1] = int.Parse(tokens[2]);
-                }
+        private static (int, int, int, bool)[] _nodes;
 
-                Thread newThread = new Thread(BigStackMain, 100000000);
-                newThread.Start();
+        private static Dictionary<int, int> _positionsIndicator;
+
+        private static int FindElement(int element)
+        {
+            if (_positionsIndicator.ContainsKey(element))
+            {
+                return _positionsIndicator[element];
             }
             else
             {
-                output.WriteLine(0);
-                output.Close();
+                return -1;
             }
+        }
+
+        private static void DeleteNodeValue(int value)
+        {
+            int index = FindElement(value);
+            if (index != -1)
+            {
+                DeleteNodeIndex(_nodes[index].Item2 - 1);
+                DeleteNodeIndex(_nodes[index].Item3 - 1);
+                _nodes[index].Item4 = false;
+                _positionsIndicator.Remove(value);
+            }
+        }
+
+        private static void DeleteNodeIndex(int index)
+        {
+            if (index != -1 && _nodes[index].Item4)
+            {
+                DeleteNodeIndex(_nodes[index].Item2 - 1);
+                DeleteNodeIndex(_nodes[index].Item3 - 1);
+                _nodes[index].Item4 = false;
+                _positionsIndicator.Remove(_nodes[index].Item1);
+            }
+        }
+
+        static void Main()
+        {
+            StreamReader input = new StreamReader("input.txt");
+            int length = int.Parse(input.ReadLine());
+            _nodes = new (int, int, int, bool)[length];
+            _positionsIndicator = new Dictionary<int, int>(length);
+            string[] tokens;
+            for (int i = 0; i < length; i++)
+            {
+                tokens = input.ReadLine().Split(' ');
+                _positionsIndicator[int.Parse(tokens[0])] = i;
+                _nodes[i] = (int.Parse(tokens[0]), int.Parse(tokens[1]), int.Parse(tokens[2]), true);
+            }
+
+            input.ReadLine();
+            StreamWriter output = new StreamWriter("output.txt");
+            foreach (string value in input.ReadLine().Split(' '))
+            {
+                DeleteNodeValue(int.Parse(value));
+                output.WriteLine(_positionsIndicator.Count);
+            }
+
+            input.Close();
+            output.Close();
         }
     }
 }
