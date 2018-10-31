a85bc2f38684141c980bdb7074de67414c2741d6
Максим <kyngfy-panda@yandex.ru>
2018-11-01 02:54:15 +0500

    Инициализировал решение для 7 недели

---
+++ Seventh week/Check balance/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check_balance
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+                
+        }
+    }
+}
