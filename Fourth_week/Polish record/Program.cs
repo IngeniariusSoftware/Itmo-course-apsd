f981dcbd0eae5fb410c6a454aadc390b3e1ba58a
Максим <kyngfy-panda@yandex.ru>
2018-11-07 11:59:20 +0500

    Добавил решение для польской записи
    Вычисление конечного значения для выражения записанного польской записью

---
+++ Fourth_week/Polish record/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Polish_record
+{
+    using System.IO;
+
+    class Program
+    {
+        static void Main()
+        {
+            StreamReader input = new StreamReader("input.txt");
+            int length = int.Parse(input.ReadLine());
+            long[] stack = new long[length];
+            int currentLength = -1;
+            foreach (string element in input.ReadLine().Split(' '))
+            {
+                switch (element)
+                {
+                    case "+":
+                        {
+                            stack[currentLength - 1] = stack[currentLength - 1] + stack[currentLength];
+                            --currentLength;
+                            break;
+                        }
+
+                    case "-":
+                        {
+                            stack[currentLength - 1] = stack[currentLength - 1] - stack[currentLength];
+                            --currentLength;
+                            break;
+                        }
+                    
+                    case "*":
+                        {
+                            stack[currentLength - 1] = stack[currentLength - 1] * stack[currentLength];
+                            --currentLength;
+                            break;
+                        }
+
+                    default:
+                        {
+                            ++currentLength;
+                            stack[currentLength] = long.Parse(element);
+                            break;
+                        }
+                }
+            }
+
+            input.Close();
+            StreamWriter output = new StreamWriter("output.txt");
+            output.WriteLine(stack[0]);
+            output.Close();
+        }
+    }
+}
