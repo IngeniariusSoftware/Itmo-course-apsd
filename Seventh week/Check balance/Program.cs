16f75620158169bd81159e9f619dd22ee3a59e02
Максим <kyngfy-panda@yandex.ru>
2018-11-17 23:31:21 +0500

    Добавил решение для проверки баланса дерева

--- Seventh week/Check balance/Program.cs
+++ Seventh week/Check balance/Program.cs
@@ -1,20 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Check_balance
+﻿namespace Check_balance
 {
+    using System;
     using System.IO;
+    using System.Threading;
 
     class Program
     {
+        private static (int, int, int)[] nodes;
+                
+        private static StreamWriter output = new StreamWriter("output.txt");
 
-
-        public static void Main(string[] args)
+        private static void BigStackMain()
         {
-            
+            Height_tree(0);
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                output.WriteLine(NodeBalance(i));
+            }
+
+            output.Close();
+        }
+
+        private static int Height_tree(int numberNode)
+        {
+            int leftHeight = 0, rightHeight = 0;
+            if (nodes[numberNode].Item1 != 0)
+            {
+                leftHeight = Height_tree(nodes[numberNode].Item1 - 1);
+            }
+
+            if (nodes[numberNode].Item2 != 0)
+            {
+                rightHeight = Height_tree(nodes[numberNode].Item2 - 1);
+            }
+
+            nodes[numberNode].Item3 = Math.Max(leftHeight, rightHeight) + 1;
+            return Math.Max(leftHeight, rightHeight) + 1;
+        }
+
+        public static int NodeBalance(int numberNode)
+        {
+            int leftHeight = 0, rightHeight = 0;
+            if (nodes[numberNode].Item1 != 0)
+            {
+                leftHeight = nodes[nodes[numberNode].Item1 - 1].Item3;
+            }
+
+            if (nodes[numberNode].Item2 != 0)
+            {
+                rightHeight = nodes[nodes[numberNode].Item2 - 1].Item3;
+            }
+
+            return rightHeight - leftHeight;
+        }
+
+        static void Main()
+        {
+            StreamReader input = new StreamReader("input.txt");
+            int length = int.Parse(input.ReadLine());
+            nodes = new (int, int, int)[length];
+            string[] tokens;
+            for (int i = 0; i < length; i++)
+            {
+                tokens = input.ReadLine().Split(' ');
+                nodes[i] = (int.Parse(tokens[1]), int.Parse(tokens[2]), 0);
+            }
+
+            input.Close();
+            Thread newThread = new Thread(BigStackMain, 100000000);
+            newThread.Start();
         }
     }
 }
