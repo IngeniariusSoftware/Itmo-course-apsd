705560f49b504e096f2b0c8ae30a730267a3c0da
Максим <kyngfy-panda@yandex.ru>
2018-12-03 22:04:19 +0500

    Инициализировал решение для декомпозиции строки

---
+++ Tenth_week/Decomposition/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Decomposition
+{
+    using System.IO;
+
+    class Program
+    {
+        private static int[] BuildPrefix(string str)
+        {
+            int[] prefixes = new int[str.Length];
+            int i = 1, j = 0;
+            while (i < str.Length)
+            {
+                if (str[i] == str[j])
+                {
+                    prefixes[i] = j + 1;
+                    ++i;
+                    ++j;
+                }
+                else
+                {
+                    if (j > 0)
+                    {
+                        j = prefixes[j - 1];
+                    }
+                    else
+                    {
+                        prefixes[i] = 0;
+                        ++i;
+                    }
+                }
+            }
+
+            return prefixes;
+        }
+
+        static void Main(string[] args)
+        {
+            StreamReader input = new StreamReader("input.txt");
+            string str = input.ReadLine();
+            input.Close();
+            for (int i = 0; i < UPPER; i++)
+            {
+                
+            }
+
+            int[] prefixes = BuildPrefix(input.ReadLine());
+            input.Close();
+            StreamWriter output = new StreamWriter("output.txt");
+            for (int i = 0; i < prefixes.Length; i++)
+            {
+                output.Write(prefixes[i] + " ");
+            }
+
+            output.Close();
+
+        }
+    }
+}
