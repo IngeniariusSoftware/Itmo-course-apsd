25213c9a3cfa95919f0d129210cd74fa0adf3414
Максим <kyngfy-panda@yandex.ru>
2018-12-03 22:03:41 +0500

    Добавил реализацию префикс функции

---
+++ Tenth_week/Prefix/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Prefix
+{
+    using System.IO;
+
+    class Program
+    {
+        private static int[] BuildPrefix(string str)
+        {
+            int[] prefixes = new int[str.Length];
+            int i = 1, j = 0;
+            while (i < str.Length)
+            {
+                if (str[i] == str[j])
+                {
+                    prefixes[i] = j + 1;
+                    ++i;
+                    ++j;
+                }
+                else
+                {
+                    if (j > 0)
+                    {
+                        j = prefixes[j - 1];
+                    }
+                    else
+                    {
+                        prefixes[i] = 0;
+                        ++i;
+                    }
+                }
+            }
+
+            return prefixes;
+        }
+
+        static void Main()
+        {
+            StreamReader input = new StreamReader("input.txt");
+            int[] prefixes = BuildPrefix(input.ReadLine());
+            input.Close();
+            StreamWriter output = new StreamWriter("output.txt");
+            for (int i = 0; i < prefixes.Length; i++)
+            {
+                output.Write(prefixes[i] + " ");
+            }
+
+            output.Close();
+        }
+    }
+}
